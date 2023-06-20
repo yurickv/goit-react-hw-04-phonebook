0bc5a2717a73e71eba88f1e578b4cc812721d1de
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-06-20 16:01:48 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm'
 import { Filter } from './Filter/Filter'
 import { ContactList } from './ContactList/ContactList'
 
-export class App extends React.Component {
+const LS_KEY = 'contacts'
 
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: ''
-  }
+export function App() {
 
+  const defaultContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+  const [phonebook, setPhonebook] = useState(() => {
+    return JSON.parse(localStorage.getItem(LS_KEY)) ?? defaultContacts;
+  })
+  const [filter, setFilter] = useState('');
 
-  addContact = obj => {
-    const chackContact = this.state.contacts.find(e => {
-      return e.name.toLowerCase() === obj.name.toLowerCase();
-    });
-    if (chackContact)
-      return alert(`${chackContact.name} is already in contacts`);
-    this.setState(prev => {
-      return { contacts: [...prev.contacts, obj] };
-    });
-  };
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(phonebook))
+  }, [phonebook])
 
-  changeFilter = ({ target }) => {
-    this.setState({ filter: target.value });
-  };
+  // функція додавання контактів
+  const addContact = obj => {
+    const chackContact = phonebook.find(e => { return e.name.toLowerCase() === obj.name.toLowerCase(); });
 
-  deleteContact = id => {
-    this.setState(prev => {
-      return { contacts: prev.contacts.filter(e => e.id !== id) };
+    if (chackContact) return alert(`${chackContact.name} is already in contacts`);
+
+    setPhonebook(prev => {
+      return [...prev.contacts, ...obj];
     });
   };
 
-  visibleNumbers = () => {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(normalizedFilter);
-    });
+  const changeFilter = ({ target }) => {
+    setFilter(target.value);
   };
 
+  const deleteContact = id => {
+    setPhonebook(prev => prev.contacts.filter(e => e.id !== id));
+  };
+
+  const visibleNumbers = phonebook.filter(({ name }) => {
+    return name.toLowerCase().includes(filter.toLowerCase());
+  });
 
 
+  return (
+    <div style={appSlyle}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
 
+      <h2>Contacts</h2>
+      <Filter filter={changeFilter} />
+      <ContactList visibleContacts={visibleNumbers}
+        deleteContact={deleteContact} />
 
-  render() {
-    const getVisibleContacts = this.visibleNumbers();
-    return (
-      <div style={appSlyle}>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
+    </div>
+  )
 
-        <h2>Contacts</h2>
-        <Filter filter={this.changeFilter} />
-        <ContactList visibleContacts={getVisibleContacts}
-          deleteContact={this.deleteContact} />
-
-      </div>
-    )
-  }
 
 };
 
